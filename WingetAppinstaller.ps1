2865a8d6348cabf1815a8dfdf7a58bd8dfd79cb7
Thatian <54353503+thatian75642@users.noreply.github.com>
2022-07-05 11:24:22 +0200

    Update WingetAppinstaller.ps1

--- WingetAppinstaller.ps1
+++ WingetAppinstaller.ps1
@@ -71,5 +71,5 @@ Remove-Item -path "C:\WingetInstall\Microsoft.DesktopAppInstaller_1.17.11601.0_x
 #Script from github
 
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/thatian75642/Winget-Task-Sequence/main/Winget%20Install%20App.ps1 -OutFile C:\WingetInstall\Winget-install.ps1
-.\Winget-install.ps1 -AppIDs notepad++.notepad++
-#.\winget-install.ps1 -AppIDs 7zip.7zip,notepad++.notepad++
+#.\Winget-install.ps1 -AppIDs notepad++.notepad++
+.\winget-install.ps1 -AppIDs 7zip.7zip,notepad++.notepad++
